7a5daeab59407c3650857987ecf44bcf36385abc
yerramreddyuday <uday.prasad@dojima.network>
2022-11-23 22:10:39 +0530

    dev: add: d11k-hermes package

---
+++ packages/d11k-hermes/README.md
@@ -0,0 +1,11 @@
+# `@d11k-ts/hermes`
+
+> TODO: description
+
+## Usage
+
+```
+const hermes = require('@d11k-ts/hermes');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/d11k-hermes/__tests__/d11k-hermes.test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const hermes = require('..')
+const assert = require('assert').strict
+
+assert.strictEqual(hermes(), 'Hello from hermes')
+console.info('hermes tests passed')
