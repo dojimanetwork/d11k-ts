86ac1ace70d8c0dd80ed77f300a53c2f844b5923
yerramreddyuday <uday.prasad@dojima.network>
2022-11-29 22:08:30 +0530

    dev: add: d11k-solana chain package

---
+++ packages/d11k-solana/README.md
@@ -0,0 +1,11 @@
+# `d11k-solana`
+
+> TODO: description
+
+## Usage
+
+```
+const d11kSolana = require('d11k-solana');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/d11k-solana/__tests__/d11k-solana.test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const d11kSolana = require('..')
+const assert = require('assert').strict
+
+assert.strictEqual(d11kSolana(), 'Hello from d11kSolana')
+console.info('d11kSolana tests passed')
