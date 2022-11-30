cdf4c692e5dc3a1acd18ac18cfb25d07ba8fe4f7
yerramreddyuday <uday.prasad@dojima.network>
2022-11-30 14:37:13 +0530

    dev: add: d11k-arweave chain package

---
+++ packages/d11k-arweave/README.md
@@ -0,0 +1,11 @@
+# `d11k-arweave`
+
+> TODO: description
+
+## Usage
+
+```
+const d11kArweave = require('d11k-arweave');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/d11k-arweave/__tests__/d11k-arweave.test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const d11kArweave = require('..')
+const assert = require('assert').strict
+
+assert.strictEqual(d11kArweave(), 'Hello from d11kArweave')
+console.info('d11kArweave tests passed')
